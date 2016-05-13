5009701248eca1b8d5b92b88ddc89aaa8bd5524b
ashokotko <ashokotko@grossum.com>
2016-05-13 12:16:13 +0300

    refactor

--- addon/helpers/ext-pop-over.js
+++ addon/helpers/ext-pop-over.js
@@ -1,12 +1,2 @@
-import Ember from 'ember';
-//import ExtPopOver from 'ember-ext-pop-over/helpers/ext-pop-over';
-//
-//export function get(obj, key) {
-//    if (arguments.length === 0) {
-//        return;
-//    }
-//    return Ember.get(obj, key);
-//}
-//
-//export default get;
+
 
