ef1610c53add6019fd1544bad08e01a11bed3e68
ashokotko <ashokotko@grossum.com>
2016-05-16 15:00:16 +0300

    temp

--- blueprints/ember-ext-pop-over/index.js
+++ blueprints/ember-ext-pop-over/index.js
@@ -1,7 +1,4 @@
 /*jshint node:true*/
 module.exports = {
-    normalizeEntityName: function() {},
-    afterInstall: function() {
-        return this.addBowerPackageToProject('ember-pop-over', '0.1.30');
-    }
+    normalizeEntityName: function() {}
 };
